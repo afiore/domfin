f2c970e4e589920ed780487282b7e3f7e5670c00
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2023-02-27 15:04:55 +0100

    add nix-shell

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.gradle_7
+    pkgs.protobuf
+    pkgs.grpc-gateway
+    pkgs.buf
+    pkgs.postman
+  ];
+  "DB_PATH" = "/home/a.fiore/.domfin.sqlite";
+  "GRPC_SERVER_PORT" = "9999";
+  "DOCKER_GATEWAY_HOST" = "172.17.0.1";
+  "TRANSACTION_SYNC_INTERVAL_MILLIS" = "300000";
+  "JAVA_HOME" = "${pkgs.jdk17}"; #Set to the same JDK used by gradle_7, so that ./gradlew script can be used
+}
